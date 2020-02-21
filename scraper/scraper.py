3396854a9d6a4f8f5dcd4559952a67fdd41034af
samg9 <9gsameer@gmail.com>
2020-02-20 18:39:34 -0800

    Add palette size to colors and clean up

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -12,7 +12,6 @@ import base64
 from time import sleep
 import random
 
-
 user_agent_list = [
     # Chrome
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
@@ -44,57 +43,30 @@ user_agent_list = [
 BASE_URL = 'https://www.instagram.com/'
 CURRENT_USER_URL = ''
 
+def get_profile(username, pal_size):
+    global PAL_SIZE
+    PAL_SIZE = pal_size
+    print("Setting pal size to:", PAL_SIZE )
+    user_url = BASE_URL+str(username)+'/'
+    CURRENT_USER_URL = user_url
+    return scrape(CURRENT_USER_URL)
 
-# Delete this function
-def get_byte_image(images_list):
-    all_encodings = []
-    for i in images_list:
-        img = Image.open(i, mode='r')
-        img_byte_arr = io.BytesIO()
-        img.save(img_byte_arr, format='PNG')
-        encoded_img = base64.encodebytes(
-            img_byte_arr.getvalue()).decode('ascii')
-        all_encodings.append(encoded_img)
-    #("len of encodings", len(all_encodings))
-    return all_encodings
-
-
-'''
-generate palette function
-tdo
-'''
-
+def get_random_user_agent():
+    return str(random.choice(user_agent_list))
 
 def get_palette_colors(url):
     response = requests.get(url,headers={'User-Agent':get_random_user_agent()})
     # print("len of response from othet GETs: ", len(response))
     image = Image.open(BytesIO(response.content))
     small_image = image.resize((80, 80))
-    # image with only 10 dominating colors
-    result = small_image.convert('P', palette=Image.ADAPTIVE, colors=5)
+    result = small_image.convert('P', palette=Image.ADAPTIVE, colors=int(PAL_SIZE))
     result.putalpha(0)
     colors = result.getcolors(80*80)      # array of colors in the image
     hexes = [rgb_to_hex(color[1][:3]) for color in colors]
     return hexes
 
-
-def get_profile(username):
-    user_url = BASE_URL+str(username)+'/'
-    #print('\n\n user url is:'+user_url)
-    CURRENT_USER_URL = user_url
-    # res = scrape(CURRENT_USER_URL)
-    return scrape(CURRENT_USER_URL)
-
-
-def get_random_user_agent():
-    return str(random.choice(user_agent_list))
-
-
 def scrape(link):
-    # print('\n\n# in scrape meothd.... about to scrape:', link)
-
     http = PoolManager()
-    # print("\n\n## about to make GET req.")
     response = http.request('GET', link, headers={
                             'User-Agent': get_random_user_agent()}
                             )
@@ -110,8 +82,6 @@ def scrape(link):
 
     display_url_scrape.extend(
         (re.findall(r'"display_url":"(.*?)"', stringy, re.IGNORECASE | re.DOTALL)))
-    # print('diaply url scrape')
-    # print(display_url_scrape)
 
     js.extend((re.findall(r'<script type="text/javascript">window._sharedData = (.*);</script>\n<script type="text/javascript">window.__initialDataLoaded',
                           stringy, re.IGNORECASE | re.DOTALL)))
@@ -121,8 +91,6 @@ def scrape(link):
     list0 = dict0['entry_data']['ProfilePage']
     user_edges = list0[0]['graphql']['user']['edge_owner_to_timeline_media']['edges']
     display_urls123 = [str(i['node']['display_url']) for i in user_edges]
-
-    #print('du2', display_urls123)
 
     response_array = []
     for url in display_urls123:
